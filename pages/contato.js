bbb1e148b0fae03578649cd9ff94a0d7b6915c38
thiagosullivan <thiagosullivan12@gmail.com>
2021-07-26 16:10:47 -0300

    changing settings of form

--- pages/contato.js
+++ pages/contato.js
@@ -7,48 +7,64 @@ import { Footer } from '../components/footer';
 import styles from '../styles/Contato.module.css';
 
 export default function Contato() {
-  const [ loading, setLoading ] = useState(false);
 
-  function handleSubmit(){
-    setLoading(true);
-  }
+  // function handleSubmit(){
+  //   setLoading(true);
+  // }
   
-  function sendEmail() {
-    const form = document.getElementById('form')
+  // function sendEmail() {
+  //   const form = document.getElementById('form')
 
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
+  //   form.addEventListener('submit', (e) => {
+  //     e.preventDefault()
 
-      const name = document.getElementById('name').value
-      const phone = document.getElementById('phone').value
-      const email = document.getElementById('email').value
-      const company = document.getElementById('company').value
-      const message = document.getElementById('message').value
+  //     const name = document.getElementById('name').value
+  //     const phone = document.getElementById('phone').value
+  //     const email = document.getElementById('email').value
+  //     const company = document.getElementById('company').value
+  //     const message = document.getElementById('message').value
     
-      fetch('/api/hello', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: name,
-          phone: phone,
-          email: email,
-          company: company,
-          message: message
-        })
-      }).then((response)=>{        
-        console.log(response)        
-        // alert('Email enviado com sucesso')
-        setLoading(false);
-        if (response.status == 200){
-          router.push('/obrigado')
-        }
-      }).catch((error) => {
-        console.log(error)
-        alert('Ocorreu um erro')
-      });
+  //     fetch('/api/hello', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({
+  //         name: name,
+  //         phone: phone,
+  //         email: email,
+  //         company: company,
+  //         message: message
+  //       })
+  //     }).then((response)=>{        
+  //       console.log(response)        
+  //       // alert('Email enviado com sucesso')
+  //       setLoading(false);
+  //       if (response.status == 200){
+  //         router.push('/obrigado')
+  //       }
+  //     }).catch((error) => {
+  //       console.log(error)
+  //       alert('Ocorreu um erro')
+  //     });
+  //   })
+  // }
+  const [ loading, setLoading ] = useState(false);
+  async function handleOnSubmit(e){
+    e.preventDefault();
+    setLoading(true);
+    const formData = {}
+    Array.from(e.currentTarget.elements).forEach(field => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    await fetch('/api/mail', {
+      method: 'post',
+      body: JSON.stringify(formData)
     })
+    router.push('/obrigado')
+
+    console.log(formData);
   }
 
   return (
@@ -59,62 +75,32 @@ export default function Contato() {
       </Head>
       <Menu />
       <MenuMobile />
-      <form onSubmit={handleSubmit} className={styles.form} id='form'>
+      <form className={styles.form} id='form' method="post" onSubmit={handleOnSubmit}>
         <h1>Formulário de Contato</h1>
         <div className={styles.formContent}>
-          <label htmlFor="name" className={styles.hiddenLabel}>
-            Nome
-          </label>
-          <input
-            id="name"
-            type="text"
-            placeholder="Seu nome"
-            required
-          />
-          <label htmlFor="phone" className={styles.hiddenLabel}>
-            Telefone
-          </label>
-          <input
-            id="phone"
-            type="text"
-            placeholder="Telefone/Whatsapp"
-            required
-          />
-          <label htmlFor="email" className={styles.hiddenLabel}>
-            E-mail
-          </label>
-          <input
-            id="email"
-            type="text"
-            placeholder="Seu E-mail"
-            required
-          />
-          <label htmlFor="company" className={styles.hiddenLabel}>
-            Empresa
-          </label>
-          <input
-            id="company"
-            type="text"
-            placeholder="Empresa (Opcional)"
-          />
-          <label htmlFor="message" className={styles.hiddenLabel}>
-            Mensagem
-          </label>
-          <textarea
-            id="message"
-            type="text"
-            placeholder="Mensagem"
-            required
-          />
-          <button onClick={sendEmail}>Enviar</button>
-        </div>
-        { loading ? (
-        <div class={styles.loading}>
-          <span>Enviando...</span>
-          <div className="spinner"></div>
-        </div>
-        ) : ''}
-        
+          <label htmlFor='name' className={styles.hiddenLabel}>Nome</label>
+          <input id="name" name="name" type="text" placeholder="Seu nome" required />
+
+          <label htmlFor="phone" className={styles.hiddenLabel}>Telefone</label>
+          <input id="phone" name="phone" type="text" placeholder="Telefone/Whatsapp" required />
+
+          <label htmlFor="email" className={styles.hiddenLabel}>E-mail</label>
+          <input id="email" name="email" type="text" placeholder="Seu E-mail" required />
+
+          <label htmlFor="company" className={styles.hiddenLabel}>Empresa</label>
+          <input id="company" name="company" type="text" placeholder="Empresa (Opcional)" />
+
+          <label htmlFor="message" className={styles.hiddenLabel}>Mensagem</label>
+          <textarea id="message" name="message" type="text" placeholder="Mensagem" required />
+
+          <button type="submit">Enviar</button>
+          { loading ? (
+          <div className="loading">
+            <span>Enviando...</span>
+            <div className="spinner"></div>
+          </div>
+          ) : ''}
+        </div>        
       </form>
       <Footer />
     </>
